862f385917b3bfc1a677dedcd5d25624cf3a3136
acce1erat0r <hiteijke@mail.ru>
2022-05-12 04:55:56 +0300

    Add: the words are taken from the file Fix: the logic of working with words with repetitive letters

--- src/main/java/MainGame.java
+++ src/main/java/MainGame.java
@@ -1,65 +1,88 @@
-import java.io.Console;
 import java.io.IOException;
-import java.sql.SQLOutput;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class MainGame {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Set<String> words = new HashSet<>();
-        Scanner scanner = new Scanner(System.in);
+
         //TODO: повторяющиеся символы и методы типа indexOf, которые возвращают первое вхождение
-        Collections.addAll(words, "black", "block", "chest", "dance", "death", "krabb");
+        List<String> words = Files.readAllLines(Paths.get("src/main/resources/dictionary.txt"));
+        Scanner scanner = new Scanner(System.in);
+        String answerWord = "staff";  // Слово которое мы загадываем
 
-        List<String> answer = Arrays.asList("b", "l", "a", "c", "k");
+        // Подсчет количества вхождений каждого символа в загаданном слове
+        List<String> answer = List.of(answerWord.split(""));
+        Map<String, Integer> frequencyAnswer = answer.stream()
+                .collect(Collectors.toMap(
+                        e -> e,
+                        e -> 1,
+                        Integer::sum
+                ));
+
+
+
 
 
 
         List<String> answerConstruct = new ArrayList<>();
-        Collections.addAll(answerConstruct, "?","?","?","?","?" );
-
-
+        Collections.addAll(answerConstruct, "?", "?", "?", "?", "?");
+        Map<String, Integer> frequencyAnswerConstruct = answer.stream()
+                .collect(Collectors.toMap(
+                        e -> e,
+                        e -> 1,
+                        Integer::sum
+                ));
 
         List<List<String>> tryingsList = new ArrayList<>();
         List<String> unknownLetter = new ArrayList<>();
 
 
-
         int trys = 0;
-        while(trys < 6){
-            System.out.println( "Write your guess");
+        while (trys < 6) {
+            System.out.println("Write your guess");
             String guessWord = scanner.next();
             List<String> guessList = List.of(guessWord.split(""));
 
+
+
             if (words.contains(guessWord.toLowerCase())) { // если слово есть в словаре
-                if (!answer.equals(guessList)) { // Если слово не равно правильному //TODO: после break все равно выводится tryingsList
+                if (!answer.equals(guessList)) { // Если слово не равно правильному
 
                     for (int j = 0; j < 5; j++) {
-                        if (answer.contains(guessList.get(j).toString().toLowerCase())) { // Если символ из введенного слова есть в ответе
+                        if (answer.contains(guessList.get(j).toLowerCase())) { // Если символ из введенного слова есть в ответе
                             if (guessList.get(j).equals(answer.get(j))) // если индекс символа из введенного слова совпадет с индексом символа в ответе
                             {
                                 answerConstruct.set(j, guessList.get(j).toUpperCase());
 
                             } else {  // если индексы символов не совпадают
-                                answerConstruct.set(j, guessList.get(j).toLowerCase());
+                                if (!answerConstruct.contains(guessList.get(j))){ // Если контейнер ответа не содержит символа то добавляем
+                                    answerConstruct.set(j, guessList.get(j).toLowerCase());
+                                } else { // А если содержит, то проверяем
+                                    if (frequencyAnswer.get(guessList.get(j)) <= frequencyAnswerConstruct.get(guessList.get(j))){
+                                        answerConstruct.set(j, guessList.get(j).toLowerCase());
+                                    }
+                                }
+
                             }
 
                         } else { // если символа нет в слове
 
-                            if (!unknownLetter.contains(guessList.get(j))){
+                            if (!unknownLetter.contains(guessList.get(j))) {
                                 unknownLetter.add(guessList.get(j));
                             }
 
                         }
-                        System.out.println();
+
 
                     }
                     tryingsList.add(new ArrayList<>(answerConstruct));
                     answerConstruct.clear();
-                    Collections.addAll(answerConstruct, "?","?","?","?","?" );
+                    Collections.addAll(answerConstruct, "?", "?", "?", "?", "?");
 
                 } else {
 
@@ -82,9 +105,6 @@ public class MainGame {
             trys++;
 
         }
-
-
-
 
 
     }
